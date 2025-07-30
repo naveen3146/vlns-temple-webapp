b983075c5a25b24858f8bf35fe19c29b3b940358
naveen3146 <naveen.mscse@gmail.com>
2025-07-29 22:03:31 -0400

    Donations page

--- src/pages/DonationPage.js
+++ src/pages/DonationPage.js
@@ -2,6 +2,9 @@ export default function DonationPage() {
   return (
     <div className="p-6">
       <h2 className="text-3xl font-bold">Make a Donation</h2>
+      <p className="mt-4 text-lg text-gray-700">
+        Please make any donations to contribute to the temple build.
+      </p>
     </div>
   );
 }
