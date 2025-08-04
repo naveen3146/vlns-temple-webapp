828085e7fbdeafd4f6ec3797b95911826dd1fa48
naveen3146 <naveen.mscse@gmail.com>
2025-08-04 02:18:02 -0400

    added loggers

--- src/components/loginService.js
+++ src/components/loginService.js
@@ -3,7 +3,11 @@ import axios from "axios";
 // Use environment variable for API URL
 const API_URL = `${process.env.REACT_APP_API_URL}/auth`;
 
+// Log the API URL to verify which endpoint is being used
+console.log("Auth API URL:", API_URL);
+
 export const login = async (username, password) => {
+  console.log("Calling login endpoint:", `${API_URL}/login`);
   const response = await axios.post(`${API_URL}/login`, { username, password });
   if (response.data.token && response.data.token.token) {
     localStorage.setItem("jwt", response.data.token.token);
@@ -12,6 +16,7 @@ export const login = async (username, password) => {
 };
 
 export const register = async (username, password) => {
+  console.log("Calling register endpoint:", `${API_URL}/register`);
   const response = await axios.post(`${API_URL}/register`, { username, password });
   return response.data;
 };
