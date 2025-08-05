adc5c506a9ff4e5cf74f3418c8ace2e657d46599
naveen3146 <naveen.mscse@gmail.com>
2025-08-05 01:49:26 -0400

    change the index file and favcon

--- src/components/loginService.js
+++ src/components/loginService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Use environment variable for API URL
-const API_URL = `https://api.sribhuvarahalakshminarasimha.in/api/auth`;
+const API_URL = `${process.env.REACT_APP_API_URL}/auth`;
 
 // Log the API URL to verify which endpoint is being used
 console.log("Auth API URL:", API_URL);
